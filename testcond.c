dcee28cd5b41b39445d069b0243daac871e98c49
Naresh-thiragabhathini <167844589+Naresh-thiragabhathini@users.noreply.github.com>
2024-04-23 17:43:19 +0530

    Create testcond.c

---
+++ testcond.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//main function
+int main()
+{
+    int age;
+     printf("enter your age");
+     scanf("%d", &age);
+     if(age >=18){
+      printf("you are eligible to vote");
+     }
+      return 0;
+}
